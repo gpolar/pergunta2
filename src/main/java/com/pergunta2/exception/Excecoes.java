59e50edb0f4709f60697c9d57a289059dddf65f9
Gustavo <Gustavo Polar>
2017-06-15 02:55:54 -0300

    v3.0

--- src/main/java/com/pergunta2/exception/Excecoes.java
+++ src/main/java/com/pergunta2/exception/Excecoes.java
@@ -2,6 +2,8 @@ package com.pergunta2.exception;
 
 import javax.validation.ConstraintViolationException;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.hateoas.VndErrors.VndError;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,7 +12,6 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import com.pergunta2.exception.http.ExcecoesHttp;
-
 /**
  * Esta classe vai ter as possiveis exeções que poderiam acontecer ao consumir
  * um serviço retornando todo no padrão application/vnd.error+json
@@ -20,32 +21,35 @@ import com.pergunta2.exception.http.ExcecoesHttp;
 @RestControllerAdvice
 public class Excecoes {
 
+	private static final Logger logger = LoggerFactory.getLogger(Excecoes.class);
+	
 	@ExceptionHandler(ConstraintViolationException.class)
 	@ResponseStatus(HttpStatus.BAD_REQUEST)
 	public VndError constraintViolationException(ConstraintViolationException exception) {
-		return criarMensagem(HttpStatus.BAD_REQUEST.value(), exception.getMessage());
+		return criarMensagem(HttpStatus.BAD_REQUEST.value(), exception);
 	}
 
 	@ExceptionHandler(IllegalStateException.class)
 	@ResponseStatus(HttpStatus.BAD_REQUEST)
 	public VndError illegalStateException(IllegalStateException exception) {
-		return criarMensagem(HttpStatus.BAD_REQUEST.value(), exception.getMessage());
+		return criarMensagem(HttpStatus.BAD_REQUEST.value(), exception);
 	}
 
 	@ExceptionHandler(IllegalArgumentException.class)
 	@ResponseStatus(HttpStatus.BAD_REQUEST)
 	public VndError illegalArgumentException(IllegalArgumentException exception) {
-		return criarMensagem(HttpStatus.BAD_REQUEST.value(), exception.getMessage());
+		return criarMensagem(HttpStatus.BAD_REQUEST.value(), exception);
 	}
 
 	/*Metodo para mostrar mais amigavel alguns erros http*/
 	@ExceptionHandler(ExcecoesHttp.class)
 	public ResponseEntity<?> excecoesHttp(ExcecoesHttp exception) {
-		return new ResponseEntity<>(criarMensagem(exception.getStatus().value(), exception.getMessage()),exception.getStatus());
+		return new ResponseEntity<>(criarMensagem(exception.getStatus().value(), exception),exception.getStatus());
 	}
 	
-	private VndError criarMensagem(Integer status, String mensagem) {
-		return new VndError(status.toString(), mensagem);
+	private VndError criarMensagem(Integer status, RuntimeException exception) {
+		logger.error(exception.getMessage(),exception);
+		return new VndError(status.toString(), exception.getMessage());
 
 	}
 
